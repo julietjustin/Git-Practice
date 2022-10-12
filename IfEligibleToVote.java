8806df7925e0d4a1768b897da19989239e584723
julietjustin <julietkurian999@gmail.com>
2022-10-12 18:28:12 +0530

    Another JAva file

---
+++ IfEligibleToVote.java
@@ -0,0 +1,15 @@
+class IfEligibleToVote{
+	public static void main(String args[]){
+		int n=60;
+			if (n>=18){
+			System.out.println("Can Vote");
+			}
+			else{
+				System.out.println("Cannot Vote");
+			}
+			
+				
+			
+		
+	}
+}
